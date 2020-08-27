b5e0a0fce5412e1c2686f723bba4e3fdb504de9f
raaaahman <sylvain@devindetails.com>
2020-08-27 12:08:55 +0200

    test(flags): Make flag tests independent from other tests

--- tests/phpunit/tests/test-flags.php
+++ tests/phpunit/tests/test-flags.php
@@ -2,6 +2,8 @@
 
 class Flags_Test extends PLL_UnitTestCase {
 
+	private $pll_env;
+
 	static function wpSetUpBeforeClass() {
 		parent::wpSetUpBeforeClass();
 
@@ -20,17 +22,22 @@ class Flags_Test extends PLL_UnitTestCase {
 	}
 
 	function setUp() {
-		self::$polylang = new PLL_Frontend( self::$polylang->links_model );
+		$options       = array_merge( PLL_Install::get_default_options(), array( 'default_lang' => 'en_US' ) );
+		$model         = new PLL_Model( $options );
+		$links_model   = new PLL_Links_Default( $model );
+		$this->pll_env = new PLL_Frontend( $links_model );
+		$this->pll_env->init();
+		$this->pll_env->model->cache->clean();
 	}
 
 	function test_default_flag() {
-		$lang = self::$polylang->model->get_language( 'en' );
+		$lang = $this->pll_env->model->get_language( 'en' );
 		$this->assertEquals( plugins_url( '/flags/us.png', POLYLANG_FILE ), $lang->get_display_flag_url() ); // Bug fixed in 2.8.1.
 		$this->assertEquals( 1, preg_match( '#<img src="data:image\/png;base64,(.+)" title="English" alt="English" width="16" height="11" style="(.+)" \/>#', $lang->get_display_flag() ) );
 	}
 
 	function test_custom_flag() {
-		$lang = self::$polylang->model->get_language( 'fr' );
+		$lang = $this->pll_env->model->get_language( 'fr' );
 		$this->assertEquals( content_url( '/polylang/fr_FR.png' ), $lang->get_display_flag_url() );
 		$this->assertEquals( '<img src="/wp-content/polylang/fr_FR.png" title="Français" alt="Français" />', $lang->get_display_flag() );
 	}
@@ -41,7 +48,7 @@ class Flags_Test extends PLL_UnitTestCase {
 	function test_default_flag_ssl() {
 		$_SERVER['HTTPS'] = 'on';
 
-		$lang = self::$polylang->model->get_language( 'en' );
+		$lang = $this->pll_env->model->get_language( 'en' );
 		$this->assertContains( 'https', $lang->get_display_flag_url() );
 
 		unset( $_SERVER['HTTPS'] );
@@ -50,7 +57,7 @@ class Flags_Test extends PLL_UnitTestCase {
 	function test_custom_flag_ssl() {
 		$_SERVER['HTTPS'] = 'on';
 
-		$lang = self::$polylang->model->get_language( 'fr' );
+		$lang = $this->pll_env->model->get_language( 'fr' );
 		$this->assertEquals( content_url( '/polylang/fr_FR.png' ), $lang->get_display_flag_url() );
 		$this->assertContains( 'https', $lang->get_display_flag_url() );
 
